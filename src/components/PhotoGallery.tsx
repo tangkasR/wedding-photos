ab6367284f88da6aa55c388e2d93c041a747aa40
tangkasR <tangkasrisdianto2@gmail.com>
2026-05-14 14:51:26 +0700

    fix lebel button unduh

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -298,7 +298,7 @@ function Lightbox({
                 d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
               />
             </svg>
-            Unduh Asli
+            Unduh Foto
           </a>
         </div>
       </div>
